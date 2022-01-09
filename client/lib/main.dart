4cbe294e5fd0ab0fc74bfaef7aff7605935da15f
Spencer Stream <sprewstream@gmail.com>
2022-01-09 17:16:48 -0600

    Setting up logic to show login or home screen whether user is logged in

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,24 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:link/screens/screens.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+final _auth = FirebaseAuth.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
   runApp(const MyApp());
 }
 
-// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeScreen(),
+      home: const Application(),
       routes: {
-        '/login': (context) => const LoginScreen(),
         '/login/signIn': (context) => const SignInScreen(),
         '/login/createAccount': (context) => const CreateAccountScreen(),
       },
     );
+  }
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser == null) {
+      return const LoginScreen();
+    }
+
+    return const HomeScreen();
   }
 }
